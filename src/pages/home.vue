41c82838eead5b00649cb21db244bd469d89be5b
阿凯 <akai@akaideMacBook-Pro.local>
2024-01-17 20:27:22 +0800

    unocss

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
+    <div class="p-4 border-gray-800 shadow-md">
         hello word
     </div>
+    <button class="... ring-2 ring-blue-500">
+        Create account
+    </button>
 </template>
 
 <script setup lang="ts">
